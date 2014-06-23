077408175744a55dff1e17ee582505afb024dbba
Chris Brown <cbrown@denimgroup.com>
2014-06-23 00:54:05 -0700

    Added queries for old and all vulns since a scan. Modified repository to use newer data model version and access old/all vuln queries. Modified service to conditionally expect three prepare_action_tickets and action_tickets methods: create, update, and close (being the actions). Added addtional comments and more standards-based method comments.

--- lib/nexpose_ticketing/queries.rb
+++ lib/nexpose_ticketing/queries.rb
@@ -13,9 +13,12 @@ module NexposeTicketing
     end
 
     # Gets all delta vulns for all sites.
-    # Returns |asset_id| |ip_address| |current_scan|  |vulnerability_id|
-    # |solution_id| |nexpose_id| |url| |summary| |fix|
-    def self.all_delta_vulns
+    #
+    # * *Returns* :
+    #   -Returns |asset_id| |ip_address| |current_scan|  |vulnerability_id||solution_id| |nexpose_id| 
+    #    |url| |summary| |fix|
+    #
+    def self.all_new_vulns
       "SELECT subs.asset_id, da.ip_address, subs.current_scan, subs.vulnerability_id, davs.solution_id, ds.nexpose_id,
        ds.url,proofAsText(ds.summary) as summary, proofAsText(ds.fix) as fix
         FROM (SELECT fasv.asset_id, fasv.vulnerability_id, s.current_scan
@@ -33,11 +36,17 @@ module NexposeTicketing
         JOIN dim_asset da ON subs.asset_id = da.asset_id
         ORDER BY da.ip_address"
     end
-
-    # Gets all delta vulns happening after reported scan id
-    # Returns |asset_id| |ip_address| |current_scan|  |vulnerability_id|
-    # |solution_id| |nexpose_id| |url| |summary| |fix|
-    def self.delta_vulns_since_scan(reported_scan)
+    
+    # Gets all new vulnerabilities happening after a reported scan id.
+    #
+    # * *Args*    :
+    #   - +reported_scan+ -  Last reported scan id.
+    #
+    # * *Returns* :
+    #   - Returns |asset_id| |ip_address| |current_scan| |vulnerability_id| |solution_id| |nexpose_id|
+    #     |url| |summary| |fix|
+    #
+    def self.new_vulns_since_scan(reported_scan)
       "SELECT subs.asset_id, da.ip_address, subs.current_scan, subs.vulnerability_id, davs.solution_id, ds.nexpose_id,
        ds.url, proofAsText(ds.summary) as summary, proofAsText(ds.fix) as fix
         FROM (SELECT fasv.asset_id, fasv.vulnerability_id, s.current_scan
@@ -46,7 +55,7 @@ module NexposeTicketing
           (
             SELECT asset_id, previousScan(asset_id) AS baseline_scan, lastScan(asset_id) AS current_scan
               FROM dim_asset) s
-              ON s.asset_id = fasv.asset_id AND (fasv.scan_id > #{reported_scan} OR fasv.scan_id = s.current_scan)
+              ON s.asset_id = fasv.asset_id AND (fasv.scan_id >= #{reported_scan} OR fasv.scan_id = s.current_scan)
               GROUP BY fasv.asset_id, fasv.vulnerability_id, s.current_scan
               HAVING baselineComparison(fasv.scan_id, current_scan) = 'New'
           ) subs
@@ -56,5 +65,87 @@ module NexposeTicketing
       AND subs.current_scan > #{reported_scan}
       ORDER BY da.ip_address"
     end
+    
+    # Gets all old vulnerabilities happening after a reported scan id.
+    #
+    # * *Args*    :
+    #   - +reported_scan+ -  Last reported scan id.
+    #
+    # * *Returns* :
+    #   - Returns |asset_id| |ip_address| |current_scan| |vulnerability_id| |solution_id| |nexpose_id|
+    #     |url| |summary| |fix|
+    #
+    def self.old_vulns_since_scan(reported_scan)
+      "SELECT subs.asset_id, da.ip_address, subs.current_scan, subs.vulnerability_id, dvs.solution_id, ds.nexpose_id, ds.url, 
+        proofAsText(ds.summary) as summary, proofAsText(ds.fix) as fix, subs.comparison
+        FROM (
+          SELECT fasv.asset_id, fasv.vulnerability_id, s.current_scan, baselineComparison(fasv.scan_id, s.current_scan) as comparison 
+          FROM fact_asset_scan_vulnerability_finding fasv
+          JOIN (
+            SELECT asset_id, previousScan(asset_id) AS baseline_scan, lastScan(asset_id) AS current_scan
+            FROM dim_asset
+          ) s ON s.asset_id = fasv.asset_id AND (fasv.scan_id >= #{reported_scan} OR fasv.scan_id = s.current_scan)
+          GROUP BY fasv.asset_id, fasv.vulnerability_id, s.current_scan
+          HAVING baselineComparison(fasv.scan_id, current_scan) = 'Old'
+        ) subs
+        JOIN dim_vulnerability_solution dvs USING (vulnerability_id)
+        JOIN dim_solution ds USING (solution_id)
+        JOIN dim_asset da ON subs.asset_id = da.asset_id
+        AND subs.current_scan > #{reported_scan}
+        ORDER BY da.ip_address"
+    end
+    
+    # Gets all vulnerabilities happening after a reported scan id. This result set also includes the 
+    # baseline comparision ("Old", "New", or "Same") allowing for IP-based ticket updating. 
+    #
+    # * *Args*    :
+    #   - +reported_scan+ -  Last reported scan id.
+    #
+    # * *Returns* :
+    #   - Returns |asset_id| |ip_address| |current_scan| |vulnerability_id| |solution_id| |nexpose_id|
+    #     |url| |summary| |fix| |comparison|
+    #
+    def self.all_vulns_since_scan(reported_scan)
+      "SELECT subs.asset_id, da.ip_address, subs.current_scan, subs.vulnerability_id, dvs.solution_id, ds.nexpose_id, ds.url, 
+        proofAsText(ds.summary) as summary, proofAsText(ds.fix) as fix, subs.comparison
+        FROM (
+          SELECT fasv.asset_id, fasv.vulnerability_id, s.current_scan, baselineComparison(fasv.scan_id, s.current_scan) as comparison 
+          FROM fact_asset_scan_vulnerability_finding fasv
+          JOIN (
+            SELECT asset_id, previousScan(asset_id) AS baseline_scan, lastScan(asset_id) AS current_scan
+            FROM dim_asset
+          ) s ON s.asset_id = fasv.asset_id AND (fasv.scan_id >= #{reported_scan} OR fasv.scan_id = s.current_scan)
+          GROUP BY fasv.asset_id, fasv.vulnerability_id, s.current_scan
+          HAVING baselineComparison(fasv.scan_id, current_scan) = 'Old'
+        ) subs
+        JOIN dim_vulnerability_solution dvs USING (vulnerability_id)
+        JOIN dim_solution ds USING (solution_id)
+        JOIN dim_asset da ON subs.asset_id = da.asset_id
+        AND subs.current_scan > #{reported_scan}
+
+        UNION
+
+        SELECT subs.asset_id, da.ip_address, subs.current_scan, subs.vulnerability_id, davs.solution_id, ds.nexpose_id, ds.url, 
+        proofAsText(ds.summary) as summary, proofAsText(ds.fix) as fix, subs.comparison
+        FROM 
+        (
+          SELECT fasv.asset_id, fasv.vulnerability_id, s.current_scan, baselineComparison(fasv.scan_id, s.current_scan) as comparison
+          FROM fact_asset_scan_vulnerability_finding fasv
+          JOIN
+          (
+            SELECT asset_id,lastScan(asset_id) AS current_scan
+            FROM dim_asset
+          ) s ON s.asset_id = fasv.asset_id AND (fasv.scan_id >= #{reported_scan} OR fasv.scan_id = s.current_scan)
+          GROUP BY fasv.asset_id, fasv.vulnerability_id, s.current_scan
+          HAVING baselineComparison(fasv.scan_id, current_scan) IN ('Same','New')
+        ) subs
+        JOIN dim_asset_vulnerability_solution davs USING (vulnerability_id)
+        JOIN dim_solution ds USING (solution_id)
+        JOIN dim_asset da ON subs.asset_id = da.asset_id
+        AND subs.current_scan > #{reported_scan}
+
+        ORDER BY ip_address, comparison"
+    end
+    
   end
 end
